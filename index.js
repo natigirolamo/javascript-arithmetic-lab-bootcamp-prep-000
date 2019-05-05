b7eac4fcca09aba0f83bd1380cb36e7afdc42e60
exuberant-glitch-6816 <natalia.girolamo@gmail.com>
2019-05-05 01:26:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
-var a, b
-
-beforeEach(function() {
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
-})
-
-it('add(a, b) adds two numbers and returns the result', function() {
-  expect(add(a, b)).toEqual(a + b)
-})
-
-it('subtract(a, b) subtracts b from a and returns the result', function() {
-  expect(subtract(a, b)).toEqual(a - b)
-})
-
-it('multiply(a, b) multiplies two numbers and returns the result', function() {
-  expect(multiply(a, b)).toEqual(a * b)
-})
-
 function add(a,b) {
   number = a+b; 
+  return number;
+}
+function subtract(a,b) {
+  number = a-b; 
+  return number;
+}
+function multiply(a,b) {
+  number = a*b; 
+  return number;
+}
+function divide(a,b) {
+  number = a/b; 
   return number;
 }
